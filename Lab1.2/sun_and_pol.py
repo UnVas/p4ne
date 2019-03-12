c6d50636c4c3c8e99557ede9e2401d49b41522ec
UnVas <uncle.vasya@gmail.com>
2019-03-12 17:23:35 +0300

    Second commit

---
+++ Lab1.2/sun_and_pol.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot
+from openpyxl import load_workbook
+
+wb = load_workbook('data_analysis_lab.xlsx')
+sheet = wb['Data']
+#sheet['A'][1:]
+
+def getvalue(x): return x.value
+
+list_x = list(map(getvalue, sheet['A'][1:]))
+list_y1 = list(map(getvalue, sheet['C'][1:]))
+list_y2 = list(map(getvalue, sheet['D'][1:]))
+
+pyplot.plot(list_x, list_y1, label='Уровень отношений')
+pyplot.plot(list_x, list_y2, label='Активность Солнца')
+pyplot.show()
