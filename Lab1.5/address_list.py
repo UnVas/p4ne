d0f385c60ec9a1faea0ebbeb62633792c03b9aad
UnVas <uncle.vasya@gmail.com>
2019-03-15 16:45:59 +0300

    Lab 1.5 second commit

--- Lab1.5/address_list.py
+++ Lab1.5/address_list.py
@@ -1,5 +1,4 @@
 import glob
-#from ipaddress import IPv4Network
 
 PATTERN = 'ip address'
 
@@ -14,19 +13,12 @@ for filename in file_list:
                 address_list.append(line.strip())
                 #print(line)
 
-
-
-
-
 list1 = list(set(address_list))
 list2 = []
 for line in list1:
     if line.find(PATTERN) == 0:
-        list2.append(line)
-
-
-
-
+        if line[11] in "0123456789":
+            list2.append(line)
 
 display_list = list2
 
