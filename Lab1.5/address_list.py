e46f5d8f119f34669f62a9d8d05301856535501e
UnVas <uncle.vasya@gmail.com>
2019-03-15 16:37:43 +0300

    Lab 1.5 first commit

---
+++ Lab1.5/address_list.py
@@ -0,0 +1,35 @@
+import glob
+#from ipaddress import IPv4Network
+
+PATTERN = 'ip address'
+
+file_list = glob.glob('config_files\*')
+address_list=[]
+
+for filename in file_list:
+    #print(filename)
+    with open(filename, 'r') as file:
+        for line in file:
+            if line.find(PATTERN) != -1:
+                address_list.append(line.strip())
+                #print(line)
+
+
+
+
+
+list1 = list(set(address_list))
+list2 = []
+for line in list1:
+    if line.find(PATTERN) == 0:
+        list2.append(line)
+
+
+
+
+
+display_list = list2
+
+for line in display_list:
+    print(line)
+
